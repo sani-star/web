a2ada63c66525cf6e175cf63951f33d004fea742
sani-star <63528823+sani-star@users.noreply.github.com>
2021-04-30 18:03:56 +0800

    Add files via upload

--- database/listrik.sql
+++ database/listrik.sql
@@ -1,1 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2021 at 06:53 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `listrik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulan`
+--
+
+CREATE TABLE `bulan` (
+  `id_bulan` int(12) NOT NULL,
+  `bulan` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kode_tarif`
+--
+
+CREATE TABLE `kode_tarif` (
+  `kode_tarif` int(11) NOT NULL,
+  `daya` varchar(12) NOT NULL,
+  `tarifkwh` int(10) NOT NULL,
+  `dendatrf` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kode_tarif` int(11) NOT NULL,
+  `nometer` int(11) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `no_hp` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_bayar` int(11) NOT NULL,
+  `id_tagihan` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `id_bulan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `tahun` varchar(5) NOT NULL,
+  `jumlah_tagihan` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL,
+  `biaya_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `pembayaran`
+--
+DELIMITER $$
+CREATE TRIGGER `after_insert` AFTER INSERT ON `pembayaran` FOR EACH ROW UPDATE tagihan SET tagihan.status='Sudah Bayar' WHERE tagihan.id_tagihan=NEW.id_tagihan
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penggunaan`
+--
+
+CREATE TABLE `penggunaan` (
+  `id_penggunaan` int(12) NOT NULL,
+  `id_pelanggan` int(12) NOT NULL,
+  `id_bulan` int(12) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tahun` varchar(5) NOT NULL,
+  `meter_awal` int(12) NOT NULL,
+  `meter_akhir` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tagihan`
+--
+
+CREATE TABLE `tagihan` (
+  `id_tagihan` int(12) NOT NULL,
+  `id_penggunaan` int(12) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `id_bulan` int(12) NOT NULL,
+  `tahun` varchar(10) NOT NULL,
+  `jumlah_meter` int(12) NOT NULL,
+  `jumlah_tagihan` int(12) NOT NULL,
+  `status` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` enum('Admin','Kasir','Pelanggan') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bulan`
+--
+ALTER TABLE `bulan`
+  ADD PRIMARY KEY (`id_bulan`);
+
+--
+-- Indexes for table `kode_tarif`
+--
+ALTER TABLE `kode_tarif`
+  ADD PRIMARY KEY (`kode_tarif`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`),
+  ADD UNIQUE KEY `nometer` (`nometer`),
+  ADD UNIQUE KEY `no_hp` (`no_hp`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `kode_tarif` (`kode_tarif`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_bayar`),
+  ADD UNIQUE KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_tagihan` (`id_tagihan`);
+
+--
+-- Indexes for table `penggunaan`
+--
+ALTER TABLE `penggunaan`
+  ADD PRIMARY KEY (`id_penggunaan`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_bulan` (`id_bulan`);
+
+--
+-- Indexes for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD PRIMARY KEY (`id_tagihan`),
+  ADD KEY `id_bulan` (`id_bulan`),
+  ADD KEY `id_penggunaan` (`id_penggunaan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bulan`
+--
+ALTER TABLE `bulan`
+  MODIFY `id_bulan` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_bayar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penggunaan`
+--
+ALTER TABLE `penggunaan`
+  MODIFY `id_penggunaan` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  MODIFY `id_tagihan` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `pelanggan_ibfk_2` FOREIGN KEY (`kode_tarif`) REFERENCES `kode_tarif` (`kode_tarif`);
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_tagihan`) REFERENCES `tagihan` (`id_tagihan`);
+
+--
+-- Constraints for table `penggunaan`
+--
+ALTER TABLE `penggunaan`
+  ADD CONSTRAINT `penggunaan_ibfk_2` FOREIGN KEY (`id_bulan`) REFERENCES `bulan` (`id_bulan`),
+  ADD CONSTRAINT `penggunaan_ibfk_3` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
+
+--
+-- Constraints for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`id_penggunaan`) REFERENCES `penggunaan` (`id_penggunaan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
